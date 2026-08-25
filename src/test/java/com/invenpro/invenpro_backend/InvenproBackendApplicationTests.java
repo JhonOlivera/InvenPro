5fc395bf1229f25fb82b82d315a6e3da990e176c
JhonOlivera <jhonedwinolivera2018@gmail.com>
2026-08-25 11:59:37 -0500

    chore: proyecto base backend generado con Spring Initializr

---
+++ src/test/java/com/invenpro/invenpro_backend/InvenproBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.invenpro.invenpro_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvenproBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
